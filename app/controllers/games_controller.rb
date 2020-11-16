7968bb883fc6e4116daf9e1181a6d94bd3b50709
Grant Holley <gvholley@gmail.com>
2020-11-16 23:33:35 +0900

    Search working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,29 +1,7 @@
 class GamesController < ApplicationController
 
   def index
-  if params[:query].present?
-      sql_query = "name LIKE :query"
-      @games = Game.where(sql_query, query: "%#{params[:query]}%")
-    else
-     flash[:notice] = "No Games Found"
-    end
-  end
-
-
-  def search
-    @games = GiantBomb::Search.new().query(params[:query]).resources('game').limit(5).fetch
-    render 'index'
-  end
-
-  def create
-    @game = Game.new(game_params)
-    @game.user = current_user
-    authorize @game
-    if @game.save
-      redirect_to game_path(@game)
-    else
-      render :new
-    end
+  @games = GiantBomb::Search.new().query(params[:query]).resources('game').limit(100).fields('name').fetch
   end
 
 
