a199000fcb7709a3461e4e87e7005d7feb49a601
Grant Holley <gvholley@gmail.com>
2020-11-20 00:52:00 +0900

    Working on routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :games
+  has_many :libraries
+  has_many :library_additions, through: :libraries, source: :game
 end
