73e11843361abd575cb1c36c6f7d2b8f0760c163
Grant Holley <gvholley@gmail.com>
2020-11-21 17:40:23 +0900

    Library errors

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,14 @@
 class Game < ApplicationRecord
 has_many :libraries
 has_many :added_games, through: :libraries, source: :user
+serialize :data
+
+  def fetch_data
+    self.data = GiantBomb::Game.detail(id)
+  end
+
+  def to_giant_bomb_game
+    GiantBomb::Game.new(data)
+  end
+
 end
