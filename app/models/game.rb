2ddf07643a940d148c2bab0647e08754f28b7053
Grant Holley <gvholley@gmail.com>
2020-11-23 19:06:45 +0900

    Library data now showing

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,8 @@ has_many :added_games, through: :libraries, source: :user
 serialize :data
 
   def fetch_data
-    self.data = GiantBomb::Game.detail(id)
+    game = GiantBomb::Game.detail(id)
+    self.data = Hash[game.instance_variables.map { |var| [var.to_s[1..-1], game.instance_variable_get(var)] } ]
   end
 
   def to_giant_bomb_game
